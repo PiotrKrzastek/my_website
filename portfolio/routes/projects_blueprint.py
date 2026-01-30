11df209141b70f189c1ece7c4040433d9dd7a0f5
PiotrKrzastek <piotrkrzastek05@gmail.com>
2026-01-30 18:37:24 +0100

    feat: update about me and projects sections for better professional alignment

--- portfolio/routes/projects_blueprint.py
+++ portfolio/routes/projects_blueprint.py
@@ -4,23 +4,26 @@ projects = Blueprint("projects", __name__)
 
 projects_list = [
     {
-        "technologies": "Python | Flask | HTML | CSS | MongoDB",
-        "description": "Simple note taking app. Created it under the guidance of Jose Salvatierra during 'Web Developer Bootcamp with Flask and Python in 2024' Udemy course.",
-        "image": "noteup.png",
-        "slug": "noteup"
-    },
-    {
-        "technologies": "Python | Flask | HTML | CSS | MongoDB",
-        "description": "App for rating and tracking your watched movies. Created it under the guidance of Jose Salvatierra during 'Web Developer Bootcamp with Flask and Python in 2024' Udemy course.",
-        "image": "movies.png",
-        "slug": "movies"
-    },
-    {
+        "title": "Portfolio",
         "technologies": "HTML | CSS | Python | Flask",
         "description": "My very own portfolio website. This is the website you are currently looking at with all the information about me and my projects",
         "image": "portfolio.png",
-        "slug": "portfolio"
-    }
+        "slug": "portfolio",
+    },
+    {
+        "title": "Movies",
+        "technologies": "Python | Flask | HTML | CSS | MongoDB",
+        "description": "App for rating and tracking your watched movies. Created it under the guidance of Jose Salvatierra during 'Web Developer Bootcamp with Flask and Python in 2024' Udemy course.",
+        "image": "movies.png",
+        "slug": "movies",
+    },
+    {   
+        "title": "Note-Up",
+        "technologies": "Python | Flask | HTML | CSS | MongoDB",
+        "description": "Simple note taking app. Created it under the guidance of Jose Salvatierra during 'Web Developer Bootcamp with Flask and Python in 2024' Udemy course.",
+        "image": "noteup.png",
+        "slug": "noteup",
+    },
 ]
 
 slugs = [project["slug"] for project in projects_list]
